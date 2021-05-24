309176f100475bf1f5fe2dbfe798649be7ebfc89
Om Verma <omverma1234q@gmail.com>
2021-05-24 17:08:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl('http://www.omverma.ml/'))
+        self.browser.setUrl(QUrl('http://www.google.com'))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
